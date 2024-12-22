4a58c7f8d539ccf4aed3757a04b7dbfe3fc4f9f4
Shubhanshu Saadhiyan <shubhanshu.saadhiyaan@gmail.com>
2024-12-22 12:35:53 +0530

    test Case 1

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath, 'utf-8', (err,data)=>{
+    if(err){
+      console.error(`Error: Unable to read the file at path "${filePath}".`);
+      return;
+    }
+    console.log(data);
+  })
 }
 
+
 // TODO: Call printFileContents with the command-line argument
+const filePath = process.argv[2];
+if (!filePath) {
+  console.error("Error: Please provide a valid file path as a command-line argument.");
+  process.exit(1);
+}
+
+printFileContents(filePath);
